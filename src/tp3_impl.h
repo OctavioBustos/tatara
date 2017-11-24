7e200e045496270ceb659fa99883b987f45d3f80
Oku <octavioabustos@gmail.com>
2017-11-24 01:36:56 -0300

    Commit inicial.

---
+++ src/tp3_impl.h
@@ -0,0 +1,67 @@
+#ifndef TP3_IMPL_H
+#define TP3_IMPL_H
+
+#include "tp3.h"
+
+#include <limits>
+#include <algorithm>
+
+using namespace std;
+
+///////////////////////////////////////////////////////////////////////////////
+/// EJERCICIO 1
+////
+
+template <typename iterator, typename bucket>
+vector<bucket> generar_buckets(iterator input_begin, iterator input_end) {
+
+	// Busco el máximo en la colección y la longitud de la colección.
+	// En base a estas dos cosas calculo la cantidad de buckets. 
+	int max = int(*input_begin); 
+	int lenght = 0; 
+	auto it = input_begin;
+	for(it; it != input_end; it++){
+		if(max <= int(*it)){ max = int(*it);}
+		lenght++;
+	}
+
+	int cantBuckets = ( lenght != 0) ? max / lenght : 0;
+
+	vector<bucket> buckets(cantBuckets);
+
+	it = input_begin;
+	for(it; it != input_end; it++){
+		int bucketIndex = ( lenght != 0) ? abs(int(*it)) % lenght : 0; 
+		buckets[bucketIndex].insert(buckets[bucketIndex].end(), (*it));
+	}	
+
+	
+	// Falta hacer sorting sobre cada bucket!. 
+
+
+	return buckets;
+}
+
+/*
+template <typename bucket>
+vector<typename bucket::value_type> aplanar_buckets(const std::vector<bucket> & B) {
+    return vector<typename bucket::value_type>();
+}
+
+///////////////////////////////////////////////////////////////////////////////
+/// EJERCICIO 2
+////
+
+fajo ordenar_por_probabilidad(const fajo& falsos_conocidos, const fajo & a_ordenar) {
+    return a_ordenar;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+/// EJERCICIO 3
+////
+
+inline Matriz multiplicar_strassen(const Matriz& A, const Matriz& B, int K) {
+    return multiplicar(A,B);
+}
+*/
+#endif // TP3_IMPL_H

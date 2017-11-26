5bac3316cc2cd9c6b2ffab8c0f134d922d9985e1
Oku <octavioabustos@gmail.com>
2017-11-26 14:53:02 -0300

    Ejercicio 2 (billetes) resuelto, pero con problemas de tiempos.

--- src/tp3_impl.h
+++ src/tp3_impl.h
@@ -34,6 +34,8 @@ vector<bucket> generar_buckets(iterator input_begin, iterator input_end) {
 		buckets[bucketIndex].insert(buckets[bucketIndex].end(), (*it));
 	}
 
+// ESTO NO VA. LOS BUCKES QUEDAN ORDENADOS DEPENDIENDO DEL CONTENEDOR QUE SE USE.
+/*
 	for(int i = 0; i < buckets.size(); i++){
 
 		for(auto it = buckets[i].begin(); it != buckets[i].end(); it++){
@@ -44,6 +46,7 @@ vector<bucket> generar_buckets(iterator input_begin, iterator input_end) {
 			}
 		}
 	}
+*/
 	return buckets;
 }
 
@@ -67,19 +70,52 @@ vector<typename bucket::value_type> aplanar_buckets(const std::vector<bucket> & 
 			res.insert(res.end(), B[i].begin(), B[i].end());
 		}
 	}
-	
+
     return res;
 }
 
-/*
+
 ///////////////////////////////////////////////////////////////////////////////
 /// EJERCICIO 2
 ////
 
 fajo ordenar_por_probabilidad(const fajo& falsos_conocidos, const fajo & a_ordenar) {
-    return a_ordenar;
-}
 
+	auto falsos = generar_buckets<fajo::const_iterator, std::set<billete> >(falsos_conocidos.begin(), falsos_conocidos.end());
+
+	int primerAnioEnVector = int(*falsos[0].begin());
+	int ultimoAnioEnVector = primerAnioEnVector + falsos.size() - 1;
+
+	std::set<billete> fajoConProbActualizadas;
+
+    for (auto & b : a_ordenar){
+    	int probabilidadBillete = 0; 
+
+    	if( (int(b) >= primerAnioEnVector) and (int(b) <= ultimoAnioEnVector) ){
+    		int indiceEnVectorFalsos = int(b) - primerAnioEnVector;
+    		auto it = falsos[indiceEnVectorFalsos].find(b);
+
+    		if(it == falsos[indiceEnVectorFalsos].end()){
+    			probabilidadBillete = falsos[indiceEnVectorFalsos].size(); 
+    		}
+    		else{
+    			probabilidadBillete = b.probabilidad_falso;
+    		}
+    	}
+
+		fajoConProbActualizadas.insert(billete(b.numero_de_serie, probabilidadBillete));
+    }
+
+    fajo resultado;
+    resultado.reserve(a_ordenar.size());
+
+	for (auto it=fajoConProbActualizadas.rbegin(); it!=fajoConProbActualizadas.rend(); ++it){
+		resultado.push_back(*it);
+	}
+
+    return resultado;
+}
+/*
 ///////////////////////////////////////////////////////////////////////////////
 /// EJERCICIO 3
 ////

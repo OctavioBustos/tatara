466c3808271a87bef17fa1e82e629bf0be4cb718
Oku <octavioabustos@gmail.com>
2017-11-27 23:05:48 -0300

    Ej 3. implementado.

--- src/tp3_impl.h
+++ src/tp3_impl.h
@@ -34,19 +34,6 @@ vector<bucket> generar_buckets(iterator input_begin, iterator input_end) {
 		buckets[bucketIndex].insert(buckets[bucketIndex].end(), (*it));
 	}
 
-// ESTO NO VA. LOS BUCKES QUEDAN ORDENADOS DEPENDIENDO DEL CONTENEDOR QUE SE USE.
-/*
-	for(int i = 0; i < buckets.size(); i++){
-
-		for(auto it = buckets[i].begin(); it != buckets[i].end(); it++){
-			for(auto it2 = it; it2 != buckets[i].end(); it2++){
-				if(*it > *it2){
-					std::swap(it, it2);
-				}
-			}
-		}
-	}
-*/
 	return buckets;
 }
 
@@ -70,7 +57,6 @@ vector<typename bucket::value_type> aplanar_buckets(const std::vector<bucket> & 
 			res.insert(res.end(), B[i].begin(), B[i].end());
 		}
 	}
-
     return res;
 }
 
@@ -81,27 +67,32 @@ vector<typename bucket::value_type> aplanar_buckets(const std::vector<bucket> & 
 
 fajo ordenar_por_probabilidad(const fajo& falsos_conocidos, const fajo & a_ordenar) {
 
+    // ORDENO FALSOS_CONOCIDOS EN BUCKETS PARA PODER BUSCAR BILLETES POR AÑO. O(N*log(m)) 
 	auto falsos = generar_buckets<fajo::const_iterator, std::set<billete> >(falsos_conocidos.begin(), falsos_conocidos.end());
 
+    // CONSULTO EL PRIMER AÑO (PRIMER BUCKET) PARA PODER INDEXAR BIEN LA BÚSQUEDA EN falsos.
 	int primerAnioEnVector = int(*falsos[0].begin());
-	int ultimoAnioEnVector = primerAnioEnVector + falsos.size() - 1;
 
+    // fajoConProbActualizadas VA A GUARDAR (EN FORMA ORDENADA POR SER UN SET) UNA COPIA DE  a_ordenar 
+    // CON LAS PROBABILIDADES DE TODOS LOS BILLETES ACTUALIZADA.
 	std::set<billete> fajoConProbActualizadas;
 
+
     for (auto & b : a_ordenar){
-    	int probabilidadBillete = 0; 
 
-    	if( (int(b) >= primerAnioEnVector) and (int(b) <= ultimoAnioEnVector) ){
+        // LA PROBABILIDAD POR DEFECTO DE b ES probabilidad_max. 
+    	int probabilidadBillete = b.probabilidad_falso;
+
+            // SI NO ENCUENTRO EL BILLETE EN falsos, ENTONCES LE ACTUALIZO LA PROBABILIDAD 
+            // EN FUNCIÓN DEL AÑO EN EL QUE FUE IMPRESO. 
+            // INVARIANTE: EN falsos EXISTE AL MENOS UN BILLETE FALSO POR AÑO: ESTO ME PERMITE
+            // BUSCAR EN EL VECTOR CON LA SEGURIDAD DE QUE NO VA A TIRAR SEG FAULT. 
+
     		int indiceEnVectorFalsos = int(b) - primerAnioEnVector;
-    		auto it = falsos[indiceEnVectorFalsos].find(b);
 
-    		if(it == falsos[indiceEnVectorFalsos].end()){
+    		if(falsos[indiceEnVectorFalsos].find(b) == falsos[indiceEnVectorFalsos].end()){
     			probabilidadBillete = falsos[indiceEnVectorFalsos].size(); 
     		}
-    		else{
-    			probabilidadBillete = b.probabilidad_falso;
-    		}
-    	}
 
 		fajoConProbActualizadas.insert(billete(b.numero_de_serie, probabilidadBillete));
     }
@@ -109,19 +100,133 @@ fajo ordenar_por_probabilidad(const fajo& falsos_conocidos, const fajo & a_orden
     fajo resultado;
     resultado.reserve(a_ordenar.size());
 
+    // ITERO SOBRE fajoConProbActualizadas PARA CREAR EL VECTOR A DEVOLVER.
 	for (auto it=fajoConProbActualizadas.rbegin(); it!=fajoConProbActualizadas.rend(); ++it){
 		resultado.push_back(*it);
 	}
 
     return resultado;
 }
-/*
+
 ///////////////////////////////////////////////////////////////////////////////
 /// EJERCICIO 3
 ////
 
-inline Matriz multiplicar_strassen(const Matriz& A, const Matriz& B, int K) {
-    return multiplicar(A,B);
+inline void sumarMatrices(Matriz& A, Matriz& B, Matriz& C, int size) {
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            C[i][j] = A[i][j] + B[i][j];
+        }
+    }
 }
-*/
+
+inline void restarMatrices(Matriz& A, Matriz& B, Matriz& C, int size) {
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            C[i][j] = A[i][j] - B[i][j];
+        }
+    }   
+}
+
+inline void strassen(Matriz& A, Matriz& B, Matriz& C, int K) {
+    if(A.size() <= K){
+		C =  multiplicar(A,B);
+		return;
+    }else{
+        int newTam = A.size()/2;
+        vector<double> inner (newTam);
+		Matriz	a11(newTam,inner), 
+				a12(newTam,inner), 
+				a21(newTam,inner), 
+				a22(newTam,inner),
+				b11(newTam,inner), 
+				b12(newTam,inner), 
+				b21(newTam,inner), 
+				b22(newTam,inner),
+				c11(newTam,inner), 
+				c12(newTam,inner), 
+				c21(newTam,inner), 
+				c22(newTam,inner),
+				p1(newTam,inner), 
+				p2(newTam,inner), 
+				p3(newTam,inner), 
+				p4(newTam,inner), 
+				p5(newTam,inner), 
+				p6(newTam,inner), 
+				p7(newTam,inner),
+				aResult(newTam,inner), 
+				bResult(newTam,inner);
+ 
+        for (int i = 0; i < newTam; i++) {
+            for (int j = 0; j < newTam; j++) {
+                a11[i][j] = A[i][j];
+                a12[i][j] = A[i][j + newTam];
+                a21[i][j] = A[i + newTam][j];
+                a22[i][j] = A[i + newTam][j + newTam];
+ 
+                b11[i][j] = B[i][j];
+                b12[i][j] = B[i][j + newTam];
+                b21[i][j] = B[i + newTam][j];
+                b22[i][j] = B[i + newTam][j + newTam];
+            }
+        }
+ 
+ 
+        sumarMatrices(a11, a22, aResult, newTam); 
+        sumarMatrices(b11, b22, bResult, newTam); 
+        strassen(aResult, bResult, p1, newTam); 
+ 
+        sumarMatrices(a21, a22, aResult, newTam);
+        strassen(aResult, b11, p2, newTam); 
+ 
+        restarMatrices(b12, b22, bResult, newTam);
+        strassen(a11, bResult, p3, newTam); 
+ 
+        restarMatrices(b21, b11, bResult, newTam);
+        strassen(a22, bResult, p4, newTam);
+ 
+        sumarMatrices(a11, a12, aResult, newTam); 
+        strassen(aResult, b22, p5, newTam); 
+ 
+        restarMatrices(a21, a11, aResult, newTam); 
+        sumarMatrices(b11, b12, bResult, newTam); 
+        strassen(aResult, bResult, p6, newTam); 
+ 
+        restarMatrices(a12, a22, aResult, newTam); 
+        sumarMatrices(b21, b22, bResult, newTam); 
+        strassen(aResult, bResult, p7, newTam); 
+ 
+        sumarMatrices(p3, p5, c12, newTam);
+        sumarMatrices(p2, p4, c21, newTam); 
+ 
+        sumarMatrices(p1, p4, aResult, newTam);
+        sumarMatrices(aResult, p7, bResult, newTam); 
+        restarMatrices(bResult, p5, c11, newTam); 
+
+        sumarMatrices(p1, p3, aResult, newTam);
+        sumarMatrices(aResult, p6, bResult, newTam); 
+        restarMatrices(bResult, p2, c22, newTam); 
+ 
+        for (int i = 0; i < newTam ; i++) {
+            for (int j = 0 ; j < newTam ; j++) {
+                C[i][j] = c11[i][j];
+                C[i][j + newTam] = c12[i][j];
+                C[i + newTam][j] = c21[i][j];
+                C[i + newTam][j + newTam] = c22[i][j];
+            }
+        }
+    }
+}
+
+inline Matriz multiplicar_strassen(Matriz& A, Matriz& B, int K) {
+    unsigned int n = A.size();
+
+    Matriz C(n, vector<double>(n)); 
+    strassen(A,B,C,K);
+
+    return C;
+}
+
+
+
 #endif // TP3_IMPL_H

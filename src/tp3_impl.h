c3489e5e20081614bf9ec780abb5df4da5fd76b6
Oku <octavioabustos@gmail.com>
2017-12-05 00:21:41 -0300

    Actualizacion Ordenar Billetes

--- src/tp3_impl.h
+++ src/tp3_impl.h
@@ -75,8 +75,9 @@ fajo ordenar_por_probabilidad(const fajo& falsos_conocidos, const fajo & a_orden
 
     // fajoConProbActualizadas VA A GUARDAR (EN FORMA ORDENADA POR SER UN SET) UNA COPIA DE  a_ordenar 
     // CON LAS PROBABILIDADES DE TODOS LOS BILLETES ACTUALIZADA.
-	std::set<billete> fajoConProbActualizadas;
+	//std::set<billete> fajoConProbActualizadas;
 
+	std::vector<billete> fajoConProbActualizadas;
 
     for (auto & b : a_ordenar){
 
@@ -94,18 +95,15 @@ fajo ordenar_por_probabilidad(const fajo& falsos_conocidos, const fajo & a_orden
     			probabilidadBillete = falsos[indiceEnVectorFalsos].size(); 
     		}
 
-		fajoConProbActualizadas.insert(billete(b.numero_de_serie, probabilidadBillete));
+		//fajoConProbActualizadas.insert(billete(b.numero_de_serie, probabilidadBillete));
+    	fajoConProbActualizadas.push_back(billete(b.numero_de_serie, probabilidadBillete));	
     }
 
-    fajo resultado;
-    resultado.reserve(a_ordenar.size());
+	std::sort(fajoConProbActualizadas.begin(), fajoConProbActualizadas.end(), [](billete a, billete b){ 
+	return a.probabilidad_falso > b.probabilidad_falso || (a.probabilidad_falso == b.probabilidad_falso 
+	&& b.numero_de_serie < a.numero_de_serie); });
 
-    // ITERO SOBRE fajoConProbActualizadas PARA CREAR EL VECTOR A DEVOLVER.
-	for (auto it=fajoConProbActualizadas.rbegin(); it!=fajoConProbActualizadas.rend(); ++it){
-		resultado.push_back(*it);
-	}
-
-    return resultado;
+	return fajoConProbActualizadas;
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -133,88 +131,83 @@ inline void strassen(Matriz& A, Matriz& B, Matriz& C, int K) {
 		C =  multiplicar(A,B);
 		return;
     }else{
-        int newTam = A.size()/2;
-        vector<double> inner (newTam);
-		Matriz	a11(newTam,inner), 
-				a12(newTam,inner), 
-				a21(newTam,inner), 
-				a22(newTam,inner),
-				b11(newTam,inner), 
-				b12(newTam,inner), 
-				b21(newTam,inner), 
-				b22(newTam,inner),
-				c11(newTam,inner), 
-				c12(newTam,inner), 
-				c21(newTam,inner), 
-				c22(newTam,inner),
-				p1(newTam,inner), 
-				p2(newTam,inner), 
-				p3(newTam,inner), 
-				p4(newTam,inner), 
-				p5(newTam,inner), 
-				p6(newTam,inner), 
-				p7(newTam,inner),
-				aResult(newTam,inner), 
-				bResult(newTam,inner);
+        int midSize = A.size()/2;
+        vector<double> aux (midSize);
+		Matriz	a11(midSize,aux), 
+				a12(midSize,aux), 
+				a21(midSize,aux), 
+				a22(midSize,aux),
+				b11(midSize,aux), 
+				b12(midSize,aux), 
+				b21(midSize,aux), 
+				b22(midSize,aux),
+				c11(midSize,aux), 
+				c12(midSize,aux), 
+				c21(midSize,aux), 
+				c22(midSize,aux),
+				m1(midSize,aux), 
+				m2(midSize,aux), 
+				m3(midSize,aux), 
+				m4(midSize,aux), 
+				m5(midSize,aux), 
+				m6(midSize,aux), 
+				m7(midSize,aux),
+				aRes(midSize,aux), 
+				bRes(midSize,aux);
  
-        for (int i = 0; i < newTam; i++) {
-            for (int j = 0; j < newTam; j++) {
+        for (int i = 0; i < midSize; i++) {
+            for (int j = 0; j < midSize; j++) {
                 a11[i][j] = A[i][j];
-                a12[i][j] = A[i][j + newTam];
-                a21[i][j] = A[i + newTam][j];
-                a22[i][j] = A[i + newTam][j + newTam];
+                a12[i][j] = A[i][j + midSize];
+                a21[i][j] = A[i + midSize][j];
+                a22[i][j] = A[i + midSize][j + midSize];
  
                 b11[i][j] = B[i][j];
-                b12[i][j] = B[i][j + newTam];
-                b21[i][j] = B[i + newTam][j];
-                b22[i][j] = B[i + newTam][j + newTam];
+                b12[i][j] = B[i][j + midSize];
+                b21[i][j] = B[i + midSize][j];
+                b22[i][j] = B[i + midSize][j + midSize];
             }
         }
  
+        sumarMatrices(a11, a22, aRes, midSize); 
+        sumarMatrices(b11, b22, bRes, midSize); 
+        strassen(aRes, bRes, m1, midSize); 
+        sumarMatrices(a21, a22, aRes, midSize);
+        strassen(aRes, b11, m2, midSize); 
+        restarMatrices(b12, b22, bRes, midSize);
+        strassen(a11, bRes, m3, midSize); 
+        restarMatrices(b21, b11, bRes, midSize);
+        strassen(a22, bRes, m4, midSize);
+        sumarMatrices(a11, a12, aRes, midSize); 
+        strassen(aRes, b22, m5, midSize); 
+        restarMatrices(a21, a11, aRes, midSize); 
+        sumarMatrices(b11, b12, bRes, midSize); 
+        strassen(aRes, bRes, m6, midSize); 
+        restarMatrices(a12, a22, aRes, midSize); 
+        sumarMatrices(b21, b22, bRes, midSize); 
+        strassen(aRes, bRes, m7, midSize); 
+        sumarMatrices(m3, m5, c12, midSize);
+        sumarMatrices(m2, m4, c21, midSize); 
+        sumarMatrices(m1, m4, aRes, midSize);
+        sumarMatrices(aRes, m7, bRes, midSize); 
+        restarMatrices(bRes, m5, c11, midSize); 
+        sumarMatrices(m1, m3, aRes, midSize);
+        sumarMatrices(aRes, m6, bRes, midSize); 
+        restarMatrices(bRes, m2, c22, midSize); 
  
-        sumarMatrices(a11, a22, aResult, newTam); 
-        sumarMatrices(b11, b22, bResult, newTam); 
-        strassen(aResult, bResult, p1, newTam); 
- 
-        sumarMatrices(a21, a22, aResult, newTam);
-        strassen(aResult, b11, p2, newTam); 
- 
-        restarMatrices(b12, b22, bResult, newTam);
-        strassen(a11, bResult, p3, newTam); 
- 
-        restarMatrices(b21, b11, bResult, newTam);
-        strassen(a22, bResult, p4, newTam);
- 
-        sumarMatrices(a11, a12, aResult, newTam); 
-        strassen(aResult, b22, p5, newTam); 
- 
-        restarMatrices(a21, a11, aResult, newTam); 
-        sumarMatrices(b11, b12, bResult, newTam); 
-        strassen(aResult, bResult, p6, newTam); 
- 
-        restarMatrices(a12, a22, aResult, newTam); 
-        sumarMatrices(b21, b22, bResult, newTam); 
-        strassen(aResult, bResult, p7, newTam); 
- 
-        sumarMatrices(p3, p5, c12, newTam);
-        sumarMatrices(p2, p4, c21, newTam); 
- 
-        sumarMatrices(p1, p4, aResult, newTam);
-        sumarMatrices(aResult, p7, bResult, newTam); 
-        restarMatrices(bResult, p5, c11, newTam); 
 
-        sumarMatrices(p1, p3, aResult, newTam);
-        sumarMatrices(aResult, p6, bResult, newTam); 
-        restarMatrices(bResult, p2, c22, newTam); 
- 
-        for (int i = 0; i < newTam ; i++) {
-            for (int j = 0 ; j < newTam ; j++) {
+        for (int i = 0; i < midSize ; i++) {
+
+            for (int j = 0 ; j < midSize ; j++) {
                 C[i][j] = c11[i][j];
-                C[i][j + newTam] = c12[i][j];
-                C[i + newTam][j] = c21[i][j];
-                C[i + newTam][j + newTam] = c22[i][j];
+                C[i][j + midSize] = c12[i][j];
+                C[i + midSize][j] = c21[i][j];
+                C[i + midSize][j + midSize] = c22[i][j];
             }
+
         }
+
+
     }
 }
 
